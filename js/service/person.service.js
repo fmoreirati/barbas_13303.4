6f94d56423b6a328ff55c97712cdbf7b158fd70a
fmoreira <fabianomoreira.prof@gmail.com.br>
2024-11-28 21:45:30 -0300

    firebase

--- js/service/person.service.js
+++ js/service/person.service.js
@@ -1,7 +1,17 @@
+import { setPerson } from "./../infra/person.repository.js"
+
 export const personService = {
- add
+    add
 }
 
-function add(person){
-
+function add(person) {
+    setPerson(person)
+        .then(res => {
+            console.log("Adicionado: ", res)
+            alert("Adicionado com sucesso!")
+        })
+        .catch(error => {
+            console.error("Error: ", error)
+            alert("Error ao adicionar o usuário")
+        })
 }
